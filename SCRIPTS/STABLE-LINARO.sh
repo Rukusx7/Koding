96d877fca7d4fda0e4590a8cc776b6ee8c962359
Chester Kener <Cl3Kener@gmail.com>
2013-12-06 06:48:23 -0500

    Must have been asleep...
    Signed-off-by: Chester Kener <Cl3Kener@gmail.com>

--- SCRIPTS/STABLE-LINARO.sh
+++ SCRIPTS/STABLE-LINARO.sh
@@ -24,7 +24,6 @@ DEFCONFIG=cyanogenmod_hercules_defconfig
 export KERNELDIR=~/android/kernel/
 export INITRAMFS_DEST=~/android/kernel/usr/initramfs
 export PACKAGEDIR=~/android/kernel/OUT
-export INITRAMFS_SOURCE=~/android/kernel/
 export Meta=~/android/kernel/Cl3Kener/META-INF
 export Etc=~/android/kernel/Cl3Kener/etc
 export Scripts=~/android/kernel/Cl3Kener/scripts
@@ -51,11 +50,10 @@ echo "${bldcya} Remove old ramdisk ${txtrst}"
 rm $KERNELDIR/ramdisk.img.gz
 
 echo -e "${bldred} Removing pesky backup files ${txtrst}"
-cd ~/android/kernel
+cd $KERNELDIR
 find ./ -name '*~' | xargs rm
 
 echo "${bldpnk} Make the kernel ${txtrst}"
-cd $KERNELDIR
 make $DEFCONFIG
 
 echo "${bldyel} Clean Environment ${txtrst}"
@@ -86,20 +84,20 @@ if [ -e $KERNELDIR/arch/arm/boot/zImage ]; then
 	cp -R $Scripts $PACKAGEDIR
 
 	export curdate=`date "+%m-%d-%Y"`
-        cp ~/Compile.log ~/android/Logs/Completed-Uber-4.4-LINARO-$curdate.log
+        cp ~/Compile.log ~/android/Logs/Completed-STABLE-4.4-LINARO-$curdate.log
         rm ~/Compile.log
 
 	cd $PACKAGEDIR
-	rm ../UBER-Android-4.4-LINARO-Cl3Kener-Nightly*.zip\
+	rm ../STABLE-Android-4.4-LINARO-Cl3Kener-Nightly*.zip\
 	rm -R .fr-7q5stU
-	zip -r ../UBER-Android-4.4-LINARO-Cl3Kener-Nightly-$curdate.zip .
+	zip -r ../STABLE-Android-4.4-LINARO-Cl3Kener-Nightly-$curdate.zip .
 
 	echo "${bldgrn} STABLE Kernel has completed successfully!!! MAKE SURE THAT YOU REALIZE THE ZIP FILE CREATED IS NOT READY. YOU MUST APPEND THE RAMDISK MANUALLY BECAUSE THIS SERVER DOESN'T KNOW HOW ${txtrst}"
 
 else
 	echo "{$bldred} STABLE KERNEL IMAGE DID NOT BUILD PROPERLY! Check Compile log! ${txtrst}"
 	export curdate=`date "+%m-%d-%Y"`
-	cp ~/Compile.log ~/android/Logs/Failed-4.2.2-$curdate.log
+	cp ~/Compile.log ~/android/Logs/Failed-STABLE-4.4-LINARO-$curdate.log
 fi;
 
 read -p "Press ENTER to Exit"
